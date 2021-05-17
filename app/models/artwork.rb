6803b77b452c138ca4f5c9df2fd1a6406c00d205
Derek Leadbetter <dleadbetter55@gmail.com>
2021-05-17 17:13:09 -0400

    BASIRA #22 - Updating artwork model to allow internal/external notes as parameters

--- app/models/artwork.rb
+++ app/models/artwork.rb
@@ -10,7 +10,7 @@ class Artwork < ApplicationRecord
   accepts_nested_attributes_for :artwork_titles, allow_destroy: true
 
   # Resourceable attributes
-  allow_params :date_start, :date_end, :date_descriptor, :height, :width, :depth, :notes, :published,
-               :repository_work_url, :accession_number, images: [],
+  allow_params :date_start, :date_end, :date_descriptor, :height, :width, :depth, :notes_external, :notes_internal,
+               :published, :repository_work_url, :accession_number, images: [],
                artwork_titles_attributes: [:id, :title, :title_type, :notes, :primary, :_destroy]
 end
