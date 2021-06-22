c3476fc62d56dafe4de9b97f3518e9f63b170e33
Derek Leadbetter <dleadbetter55@gmail.com>
2021-06-22 07:58:39 -0400

    BASIRA #42 - Updating Airtable importer to create locations for artworks

--- app/services/airtable_importer/models/place.rb
+++ app/services/airtable_importer/models/place.rb
@@ -25,6 +25,15 @@ module AirtableImporter
             return nil if country.nil?
             COUNTRIES[country.strip&.to_sym] || country.strip
           end
+         }, {
+           attribute_name: :locations,
+           airtable_name: 'Artworks',
+           type: :foreign_keys,
+           resolve: -> (airtable_id, artwork_airtable_id) do
+             artwork = ::Artwork.find_by(airtable_id: artwork_airtable_id)
+             place = ::Place.find_by(airtable_id: airtable_id)
+             ::Location.find_or_initialize_by(place: place, locateable: artwork)
+           end
          }]
       end
 
