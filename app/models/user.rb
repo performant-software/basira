55d5289b941017de6c1b61e359aae86dec2de188
Derek Leadbetter <dleadbetter55@gmail.com>
2021-06-28 12:02:29 -0400

    BASIRA #10 - Adding /api/users API end point

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable
+  # Devise
   devise :database_authenticatable, :registerable, :recoverable, :rememberable, :trackable, :validatable
+
+  # Includes
   include DeviseTokenAuth::Concerns::User
+
+  # Resourceable parameters
+  allow_params :name, :email, :password, :password_confirmation
 end
