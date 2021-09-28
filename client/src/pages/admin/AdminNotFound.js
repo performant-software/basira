3f7470e44e46da091587ac928c89dd101a12de05
Ben Silverman <ben@performantsoftware.com>
2021-09-28 10:49:35 -0400

    BASIRA #114 - Use 'history' prop from router HOC

--- client/src/pages/admin/AdminNotFound.js
+++ client/src/pages/admin/AdminNotFound.js
@@ -1,7 +1,7 @@
 // @flow
 
 import React from 'react';
-import { withRouter, useHistory } from 'react-router-dom';
+import { withRouter } from 'react-router-dom';
 import {
   Button, Container, Message
 } from 'semantic-ui-react';
@@ -11,21 +11,18 @@ import type { Translateable } from '../../types/Translateable';
 
 type Props = Translateable;
 
-const AdminNotFound = (props: Props) => {
-  const history = useHistory();
-  return (
-    <Container>
-      <Message negative>
-        <Message.Header content={props.t('AdminNotFound.errors.notFound')} />
-        <Message.Content>{props.t('AdminNotFound.messages.notFound')}</Message.Content>
-      </Message>
-      <Button
-        basic
-        content={props.t('AdminNotFound.buttons.back')}
-        onClick={() => history.go(-2)}
-      />
-    </Container>
-  );
-};
+const AdminNotFound = (props: Props) => (
+  <Container>
+    <Message negative>
+      <Message.Header content={props.t('AdminNotFound.errors.notFound')} />
+      <Message.Content>{props.t('AdminNotFound.messages.notFound')}</Message.Content>
+    </Message>
+    <Button
+      basic
+      content={props.t('AdminNotFound.buttons.back')}
+      onClick={() => props.history.go(-2)}
+    />
+  </Container>
+);
 
 export default withRouter(withMenuBar(AdminNotFound));
