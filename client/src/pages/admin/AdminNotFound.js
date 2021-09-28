06116f16fbe1bd8b82ae33ee472b16ec48699f89
Ben Silverman <ben@performantsoftware.com>
2021-09-28 10:41:28 -0400

    BASIRA #114 - Use Semantic components, i18n in 404

--- client/src/pages/admin/AdminNotFound.js
+++ client/src/pages/admin/AdminNotFound.js
@@ -2,29 +2,28 @@
 
 import React from 'react';
 import { withRouter, useHistory } from 'react-router-dom';
-import { Container } from 'semantic-ui-react';
+import {
+  Button, Container, Message
+} from 'semantic-ui-react';
 import withMenuBar from '../../hooks/MenuBar';
-import './AdminNotFound.css';
 
-const AdminNotFound = () => {
+import type { Translateable } from '../../types/Translateable';
+
+type Props = Translateable;
+
+const AdminNotFound = (props: Props) => {
   const history = useHistory();
   return (
-    <Container
-      className='admin-not-found'
-    >
-      <h2>Page not found</h2>
-      <p>
-        Sorry, the page you attempted to reach could not be found.
-      </p>
-      <p>
-        <button
-          type='button'
-          className='link-button'
-          onClick={() => history.go(-2)}
-        >
-          Click here to return to the previous page.
-        </button>
-      </p>
+    <Container>
+      <Message negative>
+        <Message.Header content={props.t('AdminNotFound.errors.notFound')} />
+        <Message.Content>{props.t('AdminNotFound.messages.notFound')}</Message.Content>
+      </Message>
+      <Button
+        basic
+        content={props.t('AdminNotFound.buttons.back')}
+        onClick={() => history.go(-2)}
+      />
     </Container>
   );
 };
