d6310e2fe6f78fa11324efac512782941642dd76
Derek Leadbetter <dleadbetter55@gmail.com>
2023-07-06 17:58:07 -0400

    BASIRA #215 - Adding base URL config option to Axios for dev server proxy work-around

--- client/src/config/Api.js
+++ client/src/config/Api.js
@@ -3,6 +3,10 @@
 import axios from 'axios';
 import _ from 'underscore';
 
+if (process.env.NODE_ENV === 'development' && process.env.REACT_APP_API_URL) {
+  _.extend(axios.defaults, { baseURL: process.env.REACT_APP_API_URL });
+}
+
 // Sets the authentication token as a request header
 axios.interceptors.request.use((config) => {
   // Set the user authentication token
