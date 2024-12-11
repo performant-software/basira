09fdfb76f0415ae19e4b25a27b17ab3b595f7324
dleadbetter <derek@performantsoftware.com>
2024-12-11 15:39:50 -0500

    BASIRA #275 - Adding authorized_vocabulary and authorized_vocabulary_url fields to places

--- app/models/place.rb
+++ app/models/place.rb
@@ -1,5 +1,6 @@
 class Place < ApplicationRecord
   # Includes
+  include Qualifiable
   include Recordable
   include Search::Place
 
@@ -11,6 +12,7 @@ class Place < ApplicationRecord
 
   # Resourceable parameters
   allow_params :name, :place_type, :lat, :long, :city, :state, :country, :url, :database_value, :notes, :same_as,
-               :part_of, locations_attributes: [:id, :locateable_id, :locateable_type, :description, :certainty,
-                                                :notes, :_destroy, qualifications_attributes: [:id, :value_list_id, :notes, :persistent, :_destroy]]
+               :part_of, :authorized_vocabulary_url,
+               locations_attributes: [:id, :locateable_id, :locateable_type, :description, :certainty, :notes, :_destroy,
+                                      qualifications_attributes: [:id, :value_list_id, :notes, :persistent, :_destroy]]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_12_11_201922) do
+ActiveRecord::Schema[7.0].define(version: 2024_12_11_202543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
   enable_extension "plpgsql"
@@ -235,6 +235,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_12_11_201922) do
     t.datetime "updated_at", null: false
     t.bigint "created_by_id"
     t.bigint "updated_by_id"
+    t.string "authorized_vocabulary_url"
     t.index ["created_by_id"], name: "index_places_on_created_by_id"
     t.index ["updated_by_id"], name: "index_places_on_updated_by_id"
   end
