ae2b01f49c123fab136e36db8bddf339fdfaafaa
dleadbetter <derek@performantsoftware.com>
2024-12-23 11:41:15 -0500

    BASIRA #290 - Adding facet tooltip to all value list driven facets

---
+++ client/src/components/SearchFacet.js
@@ -0,0 +1,126 @@
+// @flow
+
+import { FacetList } from '@performant-software/semantic-components';
+import React, {
+  forwardRef,
+  useCallback,
+  useMemo,
+  useState
+} from 'react';
+import { useRefinementList } from 'react-instantsearch-hooks-web';
+import { Button, Popup } from 'semantic-ui-react';
+import _ from 'underscore';
+import ValueLists from '../services/ValueLists';
+import './SearchFacet.css';
+
+const MAX_SHOW_MORE_LIMIT = 1000;
+
+type Props = {
+  attribute: string,
+  defaultActive?: boolean,
+  group: string,
+  object: string,
+  showMore?: boolean,
+  title?: string,
+  toggleable: boolean
+};
+
+const SearchFacet = forwardRef((props: Props, ref: HTMLElement) => {
+  const [loaded, setLoaded] = useState(false);
+  const [valueLists, setValueLists] = useState([]);
+
+  /**
+   * Groups the facets by the name value for lookup.
+   */
+  const groupedValueLists = useMemo(() => _.indexBy(valueLists, 'human_name'), [valueLists]);
+
+  /**
+   * Returns the "comment" attribute for the value list with the passed name.
+   *
+   * @type {function(*): *}
+   */
+  const getDescription = useCallback((item) => {
+    let description;
+
+    const valueList = groupedValueLists[item.value];
+    if (valueList) {
+      description = valueList.comment;
+    }
+
+    return description;
+  }, [groupedValueLists]);
+
+  /**
+   * Fetches the value lists for the object/group when the facet becomes active.
+   *
+   * @type {(function(): void)|*}
+   */
+  const onActive = useCallback(() => {
+    if (loaded) {
+      return;
+    }
+
+    ValueLists
+      .fetchAll({ object_filter: props.object, group_filter: props.group, per_page: 0})
+      .then(({ data }) => setValueLists(data.value_lists))
+      .finally(() => setLoaded(true));
+  }, [loaded, props.group, props.object]);
+
+  /**
+   * Renders the label for the passed item.
+   *
+   * @type {unknown}
+   */
+  const renderLabel = useCallback((item) => {
+    const description = getDescription(item);
+
+    return (
+      <div
+        className='facet-label'
+      >
+        <div
+          className='label'
+        >
+          { item.label }
+          { description && (
+            <Popup
+              content={description}
+              hideOnScroll
+              mouseEnterDelay={500}
+              mouseLeaveDelay={500}
+              on='hover'
+              trigger={(
+                <Button
+                  basic
+                  className='info-button'
+                  circular
+                  size='mini'
+                  icon='info'
+                />
+              )}
+            />
+          )}
+        </div>
+        <span>{ item.count }</span>
+      </div>
+    );
+  }, [getDescription]);
+
+  return (
+    <FacetList
+      attribute={props.attribute}
+      className='search-facet'
+      defaultActive={props.defaultActive}
+      onActive={onActive}
+      ref={ref}
+      renderLabel={renderLabel}
+      showMore={props.showMore}
+      showMoreLimit={MAX_SHOW_MORE_LIMIT}
+      title={props.title}
+      toggleable={props.toggleable}
+      useRefinementList={useRefinementList}
+    />
+  );
+});
+
+export default SearchFacet;
--- client/src/components/SearchFacets.js
+++ client/src/components/SearchFacets.js
@@ -1,11 +1,6 @@
 // @flow
 
-import {
-  FacetList,
-  FacetSlider,
-  FacetToggle,
-  LinkButton
-} from '@performant-software/semantic-components';
+import { FacetSlider, FacetToggle, LinkButton } from '@performant-software/semantic-components';
 import React, { useCallback, useRef } from 'react';
 import { useTranslation } from 'react-i18next';
 import {
@@ -15,13 +10,14 @@ import {
 } from 'react-instantsearch-hooks-web';
 import { Header, Segment } from 'semantic-ui-react';
 import _ from 'underscore';
+import SearchFacet from './SearchFacet';
 import useFacetLabels from '../hooks/FacetLabels';
 import './SearchFacets.css';
 
 const MAX_LIMIT = 100;
 const MAX_SHOW_MORE_LIMIT = 1000;
 
-const SearchFacets = (props: any) => {
+const SearchFacets = () => {
   const { getLabel } = useFacetLabels();
   const { t } = useTranslation();
 
@@ -92,19 +88,22 @@ const SearchFacets = (props: any) => {
         title={getLabel('artwork.date_range_facet')}
         useRangeSlider={useRange}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.object_work_type_facet'
         defaultActive={false}
+        group='Object/Work Type'
+        object='Artwork'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('artwork.object_work_type_facet')}
         toggleable
-        useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.materials_facet'
         defaultActive={false}
+        group='Material'
+        object='Artwork'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -112,9 +111,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.techniques_facet'
         defaultActive={false}
+        group='Technique'
+        object='Artwork'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -122,7 +123,7 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.creators.display_name_facet'
         defaultActive={false}
         limit={5}
@@ -135,9 +136,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.creators.nationality_facet'
         defaultActive={false}
+        group='Nationality'
+        object='Person'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -145,7 +148,7 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.locations.name_facet'
         defaultActive={false}
         limit={5}
@@ -158,7 +161,7 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.locations.country_facet'
         defaultActive={false}
         ref={setRef}
@@ -168,7 +171,7 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.locations.state_facet'
         defaultActive={false}
         ref={setRef}
@@ -179,7 +182,7 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='artwork.locations.city_facet'
         defaultActive={false}
         ref={setRef}
@@ -201,9 +204,11 @@ const SearchFacets = (props: any) => {
         as='h3'
         content={t('Search.facets.headers.visualContext')}
       />
-      <FacetList
+      <SearchFacet
         attribute='visual_context.general_subject_facet'
         defaultActive={false}
+        group='General Subject/Genre'
+        object='Visual Context'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -211,9 +216,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='visual_context.subject_cultural_context_facet'
         defaultActive={false}
+        group='Subject Cultural Context'
+        object='Visual Context'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -221,9 +228,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='visual_context.specific_subject_facet'
         defaultActive={false}
+        group='Specific Subject/Iconography'
+        object='Visual Context'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -235,46 +244,56 @@ const SearchFacets = (props: any) => {
         as='h3'
         content={t('Search.facets.headers.documentDetails')}
       />
-      <FacetList
+      <SearchFacet
         attribute='document_format_facet'
         defaultActive={false}
+        group='Document Format'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('document_format_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='document_type_facet'
         defaultActive={false}
+        group='Document Type'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('document_type_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='orientation_facet'
         defaultActive={false}
+        group='Orientation (spine)'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('orientation_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='size_facet'
         defaultActive={false}
+        group='Size'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('size_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='aperture_facet'
         defaultActive={false}
+        group='Aperture'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('aperture_facet')}
         toggleable
@@ -284,19 +303,23 @@ const SearchFacets = (props: any) => {
         as='h3'
         content={t('Search.facets.headers.externalFeatures')}
       />
-      <FacetList
+      <SearchFacet
         attribute='binding_type_facet'
         defaultActive={false}
+        group='Binding Type'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('binding_type_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='binding_color_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('binding_color_facet')}
         toggleable
@@ -309,28 +332,34 @@ const SearchFacets = (props: any) => {
         title={getLabel('number_sewing_supports_facet')}
         useRangeSlider={useRange}
       />
-      <FacetList
+      <SearchFacet
         attribute='spine_features_facet'
         defaultActive={false}
+        group='Spine Features'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('spine_features_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='furniture_facet'
         defaultActive={false}
+        group='Furniture'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('furniture_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='fastenings_facet'
         defaultActive={false}
+        group='Fastenings'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('fastenings_facet')}
         toggleable
@@ -343,10 +372,12 @@ const SearchFacets = (props: any) => {
         title={getLabel('number_fastenings_facet')}
         useRangeSlider={useRange}
       />
-      <FacetList
+      <SearchFacet
         attribute='location_fastenings_facet'
         defaultActive={false}
+        group='Location of Fastenings'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('location_fastenings_facet')}
         toggleable
@@ -359,19 +390,23 @@ const SearchFacets = (props: any) => {
         title={getLabel('inscriptions_on_binding_facet')}
         useToggleRefinement={useToggleRefinement}
       />
-      <FacetList
+      <SearchFacet
         attribute='binding_ornamentation_facet'
         defaultActive={false}
+        group='Binding Ornamentation'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('binding_ornamentation_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='binding_iconography_facet'
         defaultActive={false}
+        group='Iconography'
         ref={setRef}
+        object='Document'
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('binding_iconography_facet')}
@@ -385,37 +420,45 @@ const SearchFacets = (props: any) => {
         title={getLabel('endband_present_facet')}
         useToggleRefinement={useToggleRefinement}
       />
-      <FacetList
+      <SearchFacet
         attribute='endband_colors_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('endband_colors_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='endband_style_facet'
         defaultActive={false}
+        group='Endband Style'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('endband_style_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='binding_relationship_facet'
         defaultActive={false}
+        group='Binding Relationship to Text Block'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('binding_relationship_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='decorated_fore_edges_facet'
         defaultActive={false}
+        group='Decorated Fore-Edges'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('decorated_fore_edges_facet')}
         toggleable
@@ -428,10 +471,12 @@ const SearchFacets = (props: any) => {
         title={getLabel('uncut_fore_edges_facet')}
         useToggleRefinement={useToggleRefinement}
       />
-      <FacetList
+      <SearchFacet
         attribute='fore_edges_color_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('fore_edges_color_facet')}
         toggleable
@@ -444,19 +489,23 @@ const SearchFacets = (props: any) => {
         title={getLabel('bookmarks_registers_facet')}
         useRangeSlider={useRange}
       />
-      <FacetList
+      <SearchFacet
         attribute='bookmark_register_color_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('bookmark_register_color_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='bookmark_style_facet'
         defaultActive={false}
+        group='Bookmark/Register Style'
         ref={setRef}
+        object='Document'
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('bookmark_style_facet')}
@@ -467,10 +516,12 @@ const SearchFacets = (props: any) => {
         as='h3'
         content={t('Search.facets.headers.internalFeatures')}
       />
-      <FacetList
+      <SearchFacet
         attribute='text_technology_facet'
         defaultActive={false}
+        group='Text Technology'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('text_technology_facet')}
         toggleable
@@ -484,10 +535,12 @@ const SearchFacets = (props: any) => {
         title={getLabel('text_columns_facet')}
         useRangeSlider={useRange}
       />
-      <FacetList
+      <SearchFacet
         attribute='page_contents_facet'
         defaultActive={false}
+        group='Page Contents'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('page_contents_facet')}
         toggleable
@@ -500,10 +553,12 @@ const SearchFacets = (props: any) => {
         title={getLabel('ruling_facet')}
         useToggleRefinement={useToggleRefinement}
       />
-      <FacetList
+      <SearchFacet
         attribute='ruling_color_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('ruling_color_facet')}
         toggleable
@@ -516,74 +571,90 @@ const SearchFacets = (props: any) => {
         title={getLabel('rubrication_facet')}
         useToggleRefinement={useToggleRefinement}
       />
-      <FacetList
+      <SearchFacet
         attribute='rubrication_color_facet'
         defaultActive={false}
+        group='Color'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('rubrication_color_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='legibility_facet'
         defaultActive={false}
+        group='Legibility'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('legibility_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='script_facet'
         defaultActive={false}
+        group='Script'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('script_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='script_type_facet'
         defaultActive={false}
+        group='Script Type'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('script_type_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='simulated_script_facet'
         defaultActive={false}
+        group='Simulated Script'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('simulated_script_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='language_facet'
         defaultActive={false}
+        group='Language'
         ref={setRef}
+        object='Document'
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('language_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='illumination_type_facet'
         defaultActive={false}
+        group='Type of Illumination'
         limit={MAX_LIMIT}
+        object='Document'
         ref={setRef}
         title={getLabel('illumination_type_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='illumination_iconography_facet'
         defaultActive={false}
+        group='Iconography'
         ref={setRef}
+        object='Document'
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('illumination_iconography_facet')}
@@ -594,19 +665,23 @@ const SearchFacets = (props: any) => {
         as='h3'
         content={t('Search.facets.headers.documentActions')}
       />
-      <FacetList
+      <SearchFacet
         attribute='actions.verb_facet'
         defaultActive={false}
+        group='Action'
         ref={setRef}
+        object='Document'
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
         title={getLabel('actions.verb_facet')}
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='actions.entity_facet'
         defaultActive={false}
+        group='Entity'
+        object='Action'
         ref={setRef}
         searchable
         showMore
@@ -615,9 +690,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='actions.entity_descriptors_facet'
         defaultActive={false}
+        group='Characteristic'
+        object='Action'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
@@ -625,9 +702,11 @@ const SearchFacets = (props: any) => {
         toggleable
         useRefinementList={useRefinementList}
       />
-      <FacetList
+      <SearchFacet
         attribute='actions.body_descriptors_facet'
         defaultActive={false}
+        group='Body'
+        object='Action'
         ref={setRef}
         showMore
         showMoreLimit={MAX_SHOW_MORE_LIMIT}
