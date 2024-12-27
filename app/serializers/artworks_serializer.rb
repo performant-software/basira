ba4bf0a1da39422b15964e5cb6fdefac18827a94
dleadbetter <derek@performantsoftware.com>
2024-12-27 06:47:11 -0500

    BASIRA #294 - Adding created_by_id to serializers

--- app/serializers/artworks_serializer.rb
+++ app/serializers/artworks_serializer.rb
@@ -4,18 +4,18 @@ class ArtworksSerializer < BaseSerializer
   include LocateableSerializer
   include NestableSerializer
 
-  index_attributes :id, :date_start, :date_end, :date_descriptor, :published, :created_at, :updated_at,
+  index_attributes :id, :date_start, :date_end, :date_descriptor, :published, :created_at, :updated_at, :created_by_id,
                     primary_title: [:id, :title, qualifications: QualificationsSerializer],
                     updated_by: UsersSerializer, created_by: UsersSerializer
 
   show_attributes :id, :date_start, :date_end, :date_descriptor, :published, :height, :width, :depth,
                   :notes_external, :notes_internal, :repository_work_url, :accession_number,
-                  :documents_count, :number_documents_visible, :created_at, :updated_at,
+                  :documents_count, :number_documents_visible, :created_at, :updated_at, :created_by_id,
                   artwork_titles: [:id, :title, :notes, :primary, qualifications: QualificationsSerializer],
                   updated_by: UsersSerializer, created_by: UsersSerializer,
                   participations: ParticipationsSerializer, qualifications: QualificationsSerializer
 
-  nested_attributes :id, primary_title: [:id, :title, qualifications: QualificationsSerializer],
+  nested_attributes :id, :created_by_id, primary_title: [:id, :title, qualifications: QualificationsSerializer],
                     primary_attachment: [:id, :file_url, :primary, :thumbnail_url],
                     children: { physical_components: PhysicalComponentsSerializer }
 end
--- app/serializers/visual_contexts_serializer.rb
+++ app/serializers/visual_contexts_serializer.rb
@@ -3,14 +3,14 @@ class VisualContextsSerializer < BaseSerializer
   include AttachableSerializer
   include NestableSerializer
 
-  index_attributes :id, :name
+  index_attributes :id, :name, :created_by_id
 
   show_attributes :id, :physical_component_id, :name, :height, :width, :depth, :notes, :beta, :created_at, :updated_at,
-                  qualifications: QualificationsSerializer
+                  :created_by_id, qualifications: QualificationsSerializer
 
   show_attributes(:artwork_id) { |visual_context| visual_context.physical_component&.artwork_id }
 
-  nested_attributes :id, :physical_component_id, :name,
+  nested_attributes :id, :physical_component_id, :name, :created_by_id,
                     primary_attachment: [:id, :file_url, :primary, :thumbnail_url],
                     children: { documents: DocumentsSerializer }
 end
